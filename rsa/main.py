eec953e5e2929fe0fa0407d3913bd7c5036f97bd
Muhammadislom <44309103+Muhammadislom@users.noreply.github.com>
2021-04-26 19:06:33 +0300

    Update

--- rsa/main.py
+++ rsa/main.py
@@ -1,8 +1,14 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys
+from random import randint
+from math import sqrt
+from itertools import count, islice
 from rsa_ui import *
-
+import sys
 import math
+
+
+def isPrime(n):
+    return n > 1 and all(n % i for i in islice(count(2), int(sqrt(n) - 1)))
 
 
 def is_coprime(x, y):
@@ -21,15 +27,33 @@ class mainRsa(QMainWindow):
         if bool(self.main.inp_val_p.text()) == False or bool(self.main.inp_val_q.text()) == False or \
                 bool(self.main.inp_val_e.text()) == False:
             self.main.consolLable.setText("Заполгите все значения (p, q, e)")
-        elif self.main.inp_val_p.text().isdigit() == False or self.main.inp_val_q.text().isdigit() == False or \
+            return
+        if self.main.inp_val_p.text().isdigit() == False or self.main.inp_val_q.text().isdigit() == False or \
                 self.main.inp_val_e.text().isdigit() == False:
             self.main.consolLable.setText("Введите челое число !")
-        elif is_coprime(int(self.main.inp_val_p.text()), int(self.main.inp_val_q.text())) == False or is_coprime(
-                int(self.main.inp_val_p.text()), int(self.main.inp_val_e.text())) == False or \
-                is_coprime(int(self.main.inp_val_q.text()), int(self.main.inp_val_e.text())) == False:
-            self.main.consolLable.setText("Значения чисел (p, q, e) не евляются взаимно простыми")
-        else:
-            self.main.consolLable.setText("Всё отлично")
+            return
+        if is_coprime(int(self.main.inp_val_p.text()), int(self.main.inp_val_q.text())) == False:
+            self.main.consolLable.setText("Значения чисел (p, q) не евляются взаимно простыми")
+            return
+        self.Fn = (int(self.main.inp_val_p.text()) - 1) * (int(self.main.inp_val_q.text()) - 1)
+        self.main.val_fn_number.setText(str(self.Fn))
+        self.n = int(self.main.inp_val_p.text()) * int(self.main.inp_val_q.text())
+        self.main.val_n_number.setText(str(self.n))
+        if is_coprime(int(self.main.inp_val_e.text()), self.Fn) == False:
+            self.main.consolLable.setText("Значения чисел (e, F(n)) не евляются взаимно простыми")
+            return
+        if int(self.main.inp_val_e.text()) > self.Fn:
+            self.main.consolLable.setText("e должно быть меньше F(n)")
+            return
+        self.main.consolLable.setText("Можете продолжать")
+        while True:
+            self.e = randint(1, 100)
+            if is_coprime(self.e, int(self.main.inp_val_p.text())) == False \
+                    or is_coprime(self.e, int(self.main.inp_val_q.text())) == False \
+                    or is_coprime(self.e, int(self.main.inp_val_e.text())) == False:
+                continue
+            else:
+                break
 
 
 if __name__ == "__main__":
