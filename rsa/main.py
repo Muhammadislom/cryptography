485599a949288b84c396af549628b41e1663aa58
Muhammadislom <44309103+Muhammadislom@users.noreply.github.com>
2021-05-04 16:03:22 +0300

    Test Update

--- rsa/main.py
+++ rsa/main.py
@@ -5,12 +5,14 @@ from rsa_ui import *
 import sys
 import math
 
-opena = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 13: 'm',
-         14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y',
-         26: 'z'}
-clousea = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13,
-           'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25,
-           'z': 26}
+clousea = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 13: 'm',
+           14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y',
+           26: 'z'}
+opena = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13,
+         'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25,
+         'z': 26}
+openText = ''
+clouseText = ''
 
 
 def isPrime(n):
@@ -27,6 +29,9 @@ class MainRsa(QMainWindow):
         self.main = Ui_RSA()
         self.main.setupUi(self)
         self.main.pnq_button.clicked.connect(self.validation)
+        self.main.shifrButton.clicked.connect(self.encryption)
+        self.main.deshifrButton.clicked.connect(self.decription)
+
         self.show()
 
     def validation(self):
@@ -54,7 +59,7 @@ class MainRsa(QMainWindow):
         if isPrime(int(self.main.inp_val_e.text())) == False:
             self.main.consolLable.setText("e не простое число")
             return
-        for i in range(1, 550):
+        for i in range(1, 100):
             if (i * int(self.main.inp_val_e.text())) % self.Fn == 1:
                 self.d = i
                 self.main.np_d_number.setText(str(self.d))
@@ -67,7 +72,15 @@ class MainRsa(QMainWindow):
         self.main.consolLable.setText("Можете продолжать")
 
     def encryption(self):
-        pass
+        global openText
+        self.main.clousText.setText(" ")
+        for i in self.main.openText.toPlainText():
+            openText = str(openText) + str(
+                clousea[(opena[i] ** int(self.main.inp_val_e.text())) % int(self.main.val_n_number.text())])
+        self.main.clousText.setText(openText)
+
+    def decription(self):
+        print(self.main.clousText.toPlainText())
 
 
 if __name__ == "__main__":
